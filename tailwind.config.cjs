64e4a66f5fc2eb16ed9383e9777d8e6be484bcd0
Linnea Visser <anna56c1@stud.kea.dk>
2023-05-18 11:29:53 +0200

    fixed spacing plus logo svgs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				"padding-header-y": "var( --space-24-32px)",
+			}
+		},
 	},
 	plugins: [],
 }
