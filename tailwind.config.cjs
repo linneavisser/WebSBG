f36e53265bae6887d52ef9145f644d0a1758a1ab
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2023-05-18 22:31:36 +0200

    first part landing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,15 +4,21 @@ module.exports = {
 	theme: {
 		extend: {
 			spacing: {
-				"space-s": "var(--space-16-24px)",
-        		"space-m": "var(--space-24-32px)",
-				"space-l": "var--space-32-48px)",
-        		"space-2xl": "var(--space-64-80px)",
-       			"space-3xl": "var(--space-80-160px)",
-				"space-4xl": "var(--space-160-200px)",
+				"p-s": "var(--space-16-24px)",
+        		"p-m": "var(--space-24-32px)",
+				"p-l": "var(--space-32-48px)",
+        		"p-2xl": "var(--space-64-80px)",
+       			"p-3xl": "var(--space-80-160px)",
+				"p-4xl": "var(--space-160-200px)",
 				"padding-header-y": "var(--space-24-32px)",
 				"gap-footer":" var(--space-64-80px)",
-			}
+				"body-x":"var(--padding-body-x)",
+			},
+
+			gridTemplateColumns: {
+				"mobile": "repeat(4, 1fr)",
+				"desktop": "repeat(12, 1fr)",	
+			  },
 		},
 	},
 	plugins: [],
